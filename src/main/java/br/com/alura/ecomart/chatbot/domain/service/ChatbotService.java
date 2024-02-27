b6b412bdfff6f2f804a7868453ee23af04dc4ec9
KievMaia <kiev@prologapp.com>
2024-02-27 17:40:45 -0300

    Altera a ChatbotService para retornar o stream da resposta.

--- src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatbotService.java
+++ src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatbotService.java
@@ -2,19 +2,28 @@ package br.com.alura.ecomart.chatbot.domain.service;
 
 import br.com.alura.ecomart.chatbot.infra.openai.DadosRequisicaoChatCompletion;
 import br.com.alura.ecomart.chatbot.infra.openai.OpenAIClient;
+import br.com.alura.ecomart.chatbot.infra.openai.OpenAIClientMapper;
+import io.reactivex.Flowable;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @Service
 public class ChatbotService {
     private final OpenAIClient client;
 
-    public ChatbotService(OpenAIClient openAIClient) {this.client = openAIClient;}
+    private final OpenAIClientMapper mapper;
 
-    public String responderPergunta(@NotNull final String pergunta) {
+    public ChatbotService(OpenAIClient openAIClient, OpenAIClientMapper mapper) {this.client = openAIClient;
+        this.mapper = mapper;
+    }
+
+    public ResponseBodyEmitter responderPergunta(@NotNull final String pergunta) {
         var promptSistema =
                 "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas " +
                         "relacionadas com o ecommerce";
-        return client.enviarRequisicaoChatCompletion(new DadosRequisicaoChatCompletion(promptSistema, pergunta));
+        final var chatCompletionChunkFlowable =
+                client.enviarRequisicaoChatCompletion(new DadosRequisicaoChatCompletion(promptSistema, pergunta));
+        return mapper.fluxoStreamResposta(chatCompletionChunkFlowable);
     }
 }
